73044c506038d39be5a0b34036894e280392d0cc
박민우 <alsd20177@gmail.com>
2023-11-01 16:52:20 +0900

    🎉  : #1 - react-query, recoil provider 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import ReactQueryProvider from '@/provider/reactQueryProvider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { RecoilRoot } from 'recoil';
 import '@styles/reset.scss';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <RecoilRoot>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </RecoilRoot>
+      </body>
     </html>
   );
 }
---
+++ src/provider/reactQueryProvider.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
+
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            suspense: true,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
