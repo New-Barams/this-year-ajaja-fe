d330356fe3708ca2abe9fe2ae945b5cd22f72ca3
nocheol <qkdl60@gmail.com>
2023-11-03 23:04:43 +0900

    ✨ : #13 - switch-button 기본 구현

---
+++ src/components/SwitchButton/SwitchButton.tsx
@@ -0,0 +1,11 @@
+import './index.scss';
+
+const SwitchButton = ({}) => {
+  return (
+    <label className="switch">
+      <input type="checkbox" />
+      <span className="slider round"></span>
+    </label>
+  );
+};
+export default SwitchButton;
