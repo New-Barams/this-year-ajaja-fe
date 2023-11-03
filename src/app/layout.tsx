6e2f72e3e92157729d8bb35bbbab10a281175006
박민우 <alsd20177@gmail.com>
2023-11-03 17:36:29 +0900

    ♻ ️ : #7 - filled icon 사용을 위한 google cdn 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
           rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
       </head>
       <body className={noto_sans.className}>
