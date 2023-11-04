a3487fc225096d90bade1c00d9273fb934293039
nocheol <qkdl60@gmail.com>
2023-11-05 03:13:52 +0900

    ✨  : #13- 상태기능 추가 및 스타일 변경

--- src/components/SwitchButton/SwitchButton.tsx
+++ src/components/SwitchButton/SwitchButton.tsx
@@ -1,9 +1,20 @@
+'use client';
+
+import { useState } from 'react';
 import './index.scss';
 
-const SwitchButton = ({}) => {
+interface SwitchButtonProps {
+  onClick: (isChecked: boolean) => void;
+}
+const SwitchButton = ({ onClick }: SwitchButtonProps) => {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const handleChange = () => {
+    setIsChecked(!isChecked);
+    onClick(!isChecked);
+  };
   return (
     <label className="switch">
-      <input type="checkbox" />
+      <input type="checkbox" checked={isChecked} onChange={handleChange} />
       <span className="slider round"></span>
     </label>
   );
