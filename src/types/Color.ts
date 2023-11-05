2f4338e33ae75118c7ff5114bce0a3667baa5039
박민우 <alsd20177@gmail.com>
2023-11-05 21:52:55 +0900

    ✨ : #7 - 공통 FontSize, Color 타입 정의

---
+++ src/types/Color.ts
@@ -0,0 +1,13 @@
+export type Color =
+  | 'white-100'
+  | 'white-200'
+  | 'white-300'
+  | 'orange-200'
+  | 'orange-300'
+  | 'green-300'
+  | 'blue-300'
+  | 'purple-300'
+  | 'gray-100'
+  | 'gray-200'
+  | 'gray-300'
+  | 'shadow';
