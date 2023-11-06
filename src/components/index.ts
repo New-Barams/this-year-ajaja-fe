db9524351f2d2d6ddf8f33852e92247ce7c3ebb4
Ahn Hyesu <linim@naver.com>
2023-11-06 14:50:17 +0900

    ✨ : #9 - Button, AjajaButton, Icon index.ts에 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Tag } from '@components/Tag/Tag';
 export { default as InputTag } from '@components/InputTag/InputTag';
+export { default as Button } from '@components/Button/Button';
+export { default as AjajaButton } from '@components/AjajaButton/AjajaButton';
+export { default as Icon } from '@components/Icon/Icon';
