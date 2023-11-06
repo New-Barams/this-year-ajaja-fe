cf462edd3d1081693e5c397a577025b34b32d86c
Ahn Hyesu <linim@naver.com>
2023-11-06 14:48:30 +0900

    ✨ : #9 - 아좌좌 버튼 공통 컴포넌트 추가

---
+++ src/components/AjajaButton/AjajaButton.tsx
@@ -0,0 +1,48 @@
+import { Icon } from '@/components';
+import { Color, FontSize } from '@/types';
+import classNames from 'classnames';
+import './index.scss';
+
+interface AjajaButtonProps {
+  background: Color;
+  color: Color;
+  fontSize: FontSize;
+  ajajaColor: Color;
+  ajajaFontSize: string;
+  onClick?: () => void;
+  children: React.ReactNode;
+}
+
+export default function AjajaButton({
+  background,
+  color,
+  fontSize,
+  ajajaColor,
+  ajajaFontSize,
+  onClick,
+  children,
+  ...props
+}: AjajaButtonProps) {
+  return (
+    <button
+      className={classNames(
+        `ajaja-button`,
+        `border-origin-${background}`,
+        `background-origin-${background}`,
+        `color-origin-${color}`,
+        `font-size-${fontSize}`,
+      )}
+      onClick={onClick}
+      {...props}>
+      <div>
+        <Icon name="AJAJA" size="2xl" color={ajajaColor} isFilled={true} />
+        <p
+          className={classNames('ajaja-name', `color-origin-${color}`)}
+          style={{ fontSize: `${ajajaFontSize}` }}>
+          아좌좌
+        </p>
+      </div>
+      {children}
+    </button>
+  );
+}
