cc52e5548cc237ea6b65d0be0289c9991f614ad0
Ahn Hyesu <linim@naver.com>
2023-11-07 12:37:45 +0900

    ✨ : #27 - 상태를 Ajaja버튼으로 전부 이동 & debounce 추가

--- src/components/AjajaButton/AjajaButton.tsx
+++ src/components/AjajaButton/AjajaButton.tsx
@@ -1,19 +1,41 @@
+'use client';
+
 import { Icon } from '@/components';
+import { useDebounce } from '@/hooks/useDebounce';
 import classNames from 'classnames';
+import { useState } from 'react';
 import './index.scss';
 
 interface AjajaButtonProps {
   isFilled: boolean;
-  onClick: () => void;
-  children: React.ReactNode;
+  ajajaCount: number;
 }
 
 export default function AjajaButton({
   isFilled,
-  onClick,
-  children,
+  ajajaCount,
   ...props
 }: AjajaButtonProps) {
+  const [count, setCount] = useState(ajajaCount);
+  const [fill, setFill] = useState(isFilled);
+  const [originalCopy, setOriginalCopy] = useState(isFilled);
+
+  const handleAjaja = () => {
+    if (fill) setCount(count - 1);
+    else setCount(count + 1);
+    setFill(!fill);
+  };
+
+  const compare = () => {
+    console.log(originalCopy, fill);
+    if (originalCopy !== fill) {
+      console.log('서버 전송 API: ', count, fill);
+      setOriginalCopy(fill);
+    }
+  };
+
+  useDebounce(compare, 500, []);
+
   return (
     <button
       className={classNames(
@@ -22,14 +44,14 @@ export default function AjajaButton({
         `color-origin-gray-300`,
         `font-size-base`,
       )}
-      onClick={onClick}
+      onClick={handleAjaja}
       {...props}>
       <div>
         <Icon
           name="AJAJA"
           size="2xl"
-          color={isFilled ? 'primary' : 'gray-100'}
-          isFilled={isFilled}
+          color={fill ? 'primary' : 'gray-100'}
+          isFilled={fill}
         />
         <p
           className={classNames('ajaja-name', `color-origin-gray-300`)}
@@ -37,7 +59,7 @@ export default function AjajaButton({
           아좌좌
         </p>
       </div>
-      {children}
+      {count}
     </button>
   );
 }
