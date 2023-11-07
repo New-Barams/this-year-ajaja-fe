052ef4d1a9d10751b3a58d2efec11271cc38bd82
박민우 <alsd20177@gmail.com>
2023-11-07 13:09:35 +0900

    💄 : #23 - TextArea 컴포넌트 class별 css 추가

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -2,6 +2,7 @@
 
 import classNames from 'classnames';
 import React, { ChangeEvent } from 'react';
+import './index.scss';
 
 type kindType = 'plan-title' | 'plan-content' | 'remind';
 
@@ -10,34 +11,47 @@ interface TextAreaProps {
   textInput: string;
   placeholder: string;
   maxLength: number;
-  onChangeText: (text: string) => void;
+  readOnly?: boolean;
+  onChangeInput: (text: string) => void;
 }
+
+const isPlanType = (kind: kindType) => {
+  // plan type인지 확인해 이때만 placeHolder 올라가도록
+  return kind === 'plan-title' || kind === 'plan-content';
+};
 
 export default function TextArea({
   kind,
   textInput,
   placeholder,
   maxLength,
-  onChangeText,
+  readOnly = false,
+  onChangeInput,
 }: TextAreaProps) {
   const handleChangeInput = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    if (1) {
-      onChangeText(event.target.value);
+    if (event.target.value.length <= maxLength) {
+      onChangeInput(event.target.value);
       console.log(placeholder);
-      console.log(maxLength);
     }
   };
 
   return (
-    <textarea
-      value={textInput}
-      onChange={handleChangeInput}
-      className={classNames(
-        'textArea',
-        { 'textArea--plan-title': kind === 'plan-title' },
-        { 'textArea--plan-content': kind === 'plan-content' },
-        { 'textArea--remind': kind === 'remind' },
-      )}
-    />
+    <div className="form-input">
+      <textarea
+        id="textArea"
+        readOnly={readOnly}
+        value={textInput}
+        onChange={handleChangeInput}
+        placeholder={placeholder}
+        className={classNames(
+          'textArea',
+          { 'textArea--plan': isPlanType(kind) }, // placeHolder animation 위해서
+          { 'textArea--plan--title': kind === 'plan-title' },
+          { 'textArea--plan--content': kind === 'plan-content' },
+          { 'textArea--remind': kind === 'remind' },
+        )}
+      />
+      <label htmlFor="textArea">{placeholder}</label>
+    </div>
   );
 }
