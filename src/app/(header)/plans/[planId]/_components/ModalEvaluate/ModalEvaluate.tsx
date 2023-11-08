997422fdf91d410dec6a44e6e8166d36c0377f44
AhnHyesu <linim@naver.com>
2023-11-08 22:03:40 +0900

    ✨ #38 - progerssbar 제외 ModalEvaluate 완성

---
+++ src/app/(header)/plans/[planId]/_components/ModalEvaluate/ModalEvaluate.tsx
@@ -0,0 +1,60 @@
+import { Button, Icon } from '@/components';
+import { useModalClose } from '@/hooks/useModalClose';
+import classNames from 'classnames';
+import { useRef } from 'react';
+import './index.scss';
+
+type ModalProps = {
+  onClickFinish: () => void;
+  onClickExit: () => void;
+  children: React.ReactNode;
+};
+
+export default function ModalEvaluate({
+  onClickFinish,
+  onClickExit,
+  children,
+}: ModalProps) {
+  const backgroundRef = useRef<HTMLDivElement | null>(null);
+  useModalClose(backgroundRef, onClickExit);
+  return (
+    <div
+      className={classNames(
+        `modal-evaluate-wrapper`,
+        'border-origin-primary',
+        'background-origin-white-100',
+      )}
+      ref={backgroundRef}>
+      <div
+        className={classNames('modal-evaluate-wrapper__exit')}
+        onClick={onClickExit}>
+        <Icon name="CLOSE" color="gray-300" size="3xl" />
+      </div>
+
+      <div className={classNames(`modal-evaluate-wrapper__content`)}>
+        <div
+          className={classNames(
+            `font-size-2xl`,
+            `color-origin-gray-300`,
+            `modal-evaluate-wrapper__text`,
+          )}>
+          {children}
+        </div>
+        <Button
+          background="primary"
+          color="white-100"
+          size="lg"
+          border={false}
+          onClick={onClickFinish}>
+          평가 완료
+        </Button>
+        <div className={classNames('modal-evaluate-wrapper__worning')}>
+          <Icon name="WARNING" color="primary" size="3xl" />
+          <span className={classNames('font-size-2xl', 'color-origin-primary')}>
+            평가가 완료되면 다시 평가할 수 없습니다.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
