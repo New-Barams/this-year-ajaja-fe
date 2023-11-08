3c8fea716cf0b2d2cfbc66e93dab04aa9e88f3b4
AhnHyesu <linim@naver.com>
2023-11-08 18:15:45 +0900

    ✨ : #34 - header layout 완성

--- src/app/(header)/layout.tsx
+++ src/app/(header)/layout.tsx
@@ -1,12 +1,25 @@
+import classNames from 'classnames';
+import Header from './_components/Header/Header';
+import './_components/index.scss';
+
 export default function HeaderLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <h2>header</h2>
-      {children}
-    </>
+    <div className={classNames('header-layout', 'background-origin-white-200')}>
+      <div className={classNames('header-layout__wrapper')}>
+        <Header />
+        <div
+          className={classNames(
+            'header-layout__content',
+            'border-origin-orange-300',
+            'border-round',
+          )}>
+          {children}
+        </div>
+      </div>
+    </div>
   );
 }
