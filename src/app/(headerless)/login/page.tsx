27e2fbc2c1f5ea3d844bdf9836f32838c9c6acd9
nocheol <qkdl60@gmail.com>
2023-11-10 22:51:20 +0900

    ✨ :#48 - 로그인 페이지 ui 구현

--- src/app/(headerless)/login/page.tsx
+++ src/app/(headerless)/login/page.tsx
@@ -1,3 +1,38 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import './index.scss';
+
 export default function LoginPage() {
-  return <h2>loginPage</h2>;
+  return (
+    <div className="wrapper">
+      <div className="login">
+        <Image
+          className="login__logo"
+          src="/this-year-ajaja-logo.svg"
+          width={260}
+          height={190}
+          alt="thisYearAjajaLogo"
+        />
+        <div className="login__sentence">
+          <p className="login__sentence--text">올해의 신년 계획을 생성하고,</p>
+          <p className="login__sentence--text">
+            주기적으로 리마인드 받아보세요!!
+          </p>
+        </div>
+        <div className="login__buttons">
+          <Link className="login__buttons--kakaoLogin" href="/login">
+            <Image
+              src="/kakao_login_large_narrow.png"
+              width={230}
+              height={60}
+              alt="kakaoLogin"
+            />
+          </Link>
+          <Link className="login__buttons--unLogin" href="/explore">
+            로그인 하지 않고 둘러보기
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
