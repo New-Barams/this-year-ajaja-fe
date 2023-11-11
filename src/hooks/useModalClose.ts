5f867d29a1f9fe6109d90e7b921c5eb757205ded
AhnHyesu <linim@naver.com>
2023-11-11 16:12:45 +0900

    ♻️ : #38 - useCallback 추가해 의존성 배열에 handle함수 추가

--- src/hooks/useModalClose.ts
+++ src/hooks/useModalClose.ts
@@ -1,20 +1,23 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 
 export const useModalClose = (
   ref: React.RefObject<HTMLElement>,
   callback: () => void,
 ) => {
-  const handleClickOutside = (e: MouseEvent) => {
-    if (ref.current && !ref.current.contains(e.target as Node)) {
-      callback();
-    }
-  };
+  const handleClickOutside = useCallback(
+    (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        callback();
+      }
+    },
+    [ref, callback],
+  );
   useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  });
+  }, [handleClickOutside]);
 };
