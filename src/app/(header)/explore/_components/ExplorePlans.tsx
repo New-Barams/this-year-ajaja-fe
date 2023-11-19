3eb2bc51ad5aee54f17ba16ec2e1efb88832ad9d
Ahn Hyesu <linim@naver.com>
2023-11-20 00:12:00 +0900

    ✨ : #53 - Plans 컴포넌트 사용 추가

--- src/app/(header)/explore/_components/ExplorePlans.tsx
+++ src/app/(header)/explore/_components/ExplorePlans.tsx
@@ -1,10 +1,11 @@
-// import { useState } from 'react';
+import Plans from './Plans/Plans';
 import Tab from './Tab/Tab';
 
 export default function ExplorePlans() {
   return (
     <div>
       <Tab />
+      <Plans />
     </div>
   );
 }
