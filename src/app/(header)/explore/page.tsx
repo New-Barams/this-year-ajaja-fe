dd8e5f785698aeb0fbf932ed693b82173faf8cfe
Ahn Hyesu <linim@naver.com>
2023-11-20 00:13:22 +0900

    💩 : #53 - 사용하지 않는 class, import 제거

--- src/app/(header)/explore/page.tsx
+++ src/app/(header)/explore/page.tsx
@@ -1,11 +1,5 @@
-import classNames from 'classnames';
 import ExplorePlans from './_components/ExplorePlans';
-import './_components/index.scss';
 
 export default function ExplorePage() {
-  return (
-    <div className={classNames('explore-plans')}>
-      <ExplorePlans />
-    </div>
-  );
+  return <ExplorePlans />;
 }
