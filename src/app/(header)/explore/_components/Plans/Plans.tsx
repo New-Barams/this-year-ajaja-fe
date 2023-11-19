cd1401547979e14666fef24a06055337981b396e
Ahn Hyesu <linim@naver.com>
2023-11-20 00:32:26 +0900

    💄 : #53 - Plans Grid 추가

--- src/app/(header)/explore/_components/Plans/Plans.tsx
+++ src/app/(header)/explore/_components/Plans/Plans.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import classNames from 'classnames';
 import Link from 'next/link';
 // import { useAllPlansQuery } from '@/hooks/apis/plans/useAllPlansQuery';
 import Card from '../Card/Card';
+import './index.scss';
 
 export default function Plans() {
   // const { allPlans } = useAllPlansQuery();
@@ -60,10 +62,160 @@ export default function Plans() {
         tags: ['태그', '태그', '태태', '태태', '태태'],
         createdAt: '2023-??',
       },
+      {
+        id: 0,
+        userId: 1,
+        nickname: '춤추는 고래',
+        title:
+          '올해도 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 ',
+        ajajas: 255,
+        tags: ['태그', '태그태그', '태태태태태태그'],
+        createdAt: '2023-??',
+      },
+      {
+        id: 1,
+        userId: 2,
+        nickname: '윈드밀하는 미어캣zooooooooooo',
+        title:
+          '올해도 아좌좌 아좌좌 아좌좌 김수한무거북이와두루미암튼모르겠는데긴이름름이름이다',
+        ajajas: 0,
+        tags: [
+          '태그',
+          '태그태그',
+          '태태태태태태그',
+          '태태태태태태그',
+          '태태태태그태태태태그',
+        ],
+        createdAt: '2023-??',
+      },
+      {
+        id: 2,
+        userId: 2,
+        nickname: '윈드밀하는 미어캣',
+        title: '올해도 아좌좌 아좌좌',
+        ajajas: 1000,
+        tags: [
+          '태그',
+          '태그태그',
+          '태태태태태태그',
+          '태태태태태태그',
+          '태태태태그태태태태그',
+        ],
+        createdAt: '2023-??',
+      },
+      {
+        id: 3,
+        userId: 2,
+        nickname: '윈드밀하는 미어캣',
+        title: '올해도 아좌좌 아좌좌',
+        ajajas: 2231,
+        tags: ['태그', '태그', '태태', '태태', '태태'],
+        createdAt: '2023-??',
+      },
+      {
+        id: 0,
+        userId: 1,
+        nickname: '춤추는 고래',
+        title:
+          '올해도 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 ',
+        ajajas: 255,
+        tags: ['태그', '태그태그', '태태태태태태그'],
+        createdAt: '2023-??',
+      },
+      {
+        id: 1,
+        userId: 2,
+        nickname: '윈드밀하는 미어캣zooooooooooo',
+        title:
+          '올해도 아좌좌 아좌좌 아좌좌 김수한무거북이와두루미암튼모르겠는데긴이름름이름이다',
+        ajajas: 0,
+        tags: [
+          '태그',
+          '태그태그',
+          '태태태태태태그',
+          '태태태태태태그',
+          '태태태태그태태태태그',
+        ],
+        createdAt: '2023-??',
+      },
+      {
+        id: 2,
+        userId: 2,
+        nickname: '윈드밀하는 미어캣',
+        title: '올해도 아좌좌 아좌좌',
+        ajajas: 1000,
+        tags: [
+          '태그',
+          '태그태그',
+          '태태태태태태그',
+          '태태태태태태그',
+          '태태태태그태태태태그',
+        ],
+        createdAt: '2023-??',
+      },
+      {
+        id: 3,
+        userId: 2,
+        nickname: '윈드밀하는 미어캣',
+        title: '올해도 아좌좌 아좌좌',
+        ajajas: 2231,
+        tags: ['태그', '태그', '태태', '태태', '태태'],
+        createdAt: '2023-??',
+      },
+      {
+        id: 0,
+        userId: 1,
+        nickname: '춤추는 고래',
+        title:
+          '올해도 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 ',
+        ajajas: 255,
+        tags: ['태그', '태그태그', '태태태태태태그'],
+        createdAt: '2023-??',
+      },
+      {
+        id: 1,
+        userId: 2,
+        nickname: '윈드밀하는 미어캣zooooooooooo',
+        title:
+          '올해도 아좌좌 아좌좌 아좌좌 김수한무거북이와두루미암튼모르겠는데긴이름름이름이다',
+        ajajas: 0,
+        tags: [
+          '태그',
+          '태그태그',
+          '태태태태태태그',
+          '태태태태태태그',
+          '태태태태그태태태태그',
+        ],
+        createdAt: '2023-??',
+      },
+      {
+        id: 2,
+        userId: 2,
+        nickname: '윈드밀하는 미어캣',
+        title: '올해도 아좌좌 아좌좌',
+        ajajas: 1000,
+        tags: [
+          '태그',
+          '태그태그',
+          '태태태태태태그',
+          '태태태태태태그',
+          '태태태태그태태태태그',
+        ],
+        createdAt: '2023-??',
+      },
+      {
+        id: 3,
+        userId: 2,
+        nickname: '윈드밀하는 미어캣',
+        title: '올해도 아좌좌 아좌좌',
+        ajajas: 2231,
+        tags: ['태그', '태그', '태태', '태태', '태태'],
+        createdAt: '2023-??',
+      },
     ],
   };
   return (
-    <>
+    <div className={classNames('plans__wrapper')}>
       {allPlans.data.map((plan, index) => {
         return (
           <Link key={index} href={`/plans/${plan.id}`}>
@@ -71,6 +223,6 @@ export default function Plans() {
           </Link>
         );
       })}
-    </>
+    </div>
   );
 }
