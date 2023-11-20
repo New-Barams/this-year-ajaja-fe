999d718ae9806ec29621ab70cae6b99d43817290
Ahn Hyesu <linim@naver.com>
2023-11-20 19:36:46 +0900

    ♻️ : #62 - API DOMAIN 상수화로 변경

--- src/apis/client/toggleIsPublic.ts
+++ src/apis/client/toggleIsPublic.ts
@@ -1,5 +1,6 @@
+import { DOMAIN } from '@/constants/api';
 import { axiosInstanceClient } from '../axiosInstanceClient';
 
 export const toggleIsPublic = (planId: number) => {
-  return axiosInstanceClient.put(`/plans/${planId}/public`);
+  return axiosInstanceClient.put(DOMAIN.PUT_PLANS_SWITCH_PUBLIC(planId));
 };
--- src/apis/server/getMyPlans.ts
+++ src/apis/server/getMyPlans.ts
@@ -1,13 +1,10 @@
+import { DOMAIN } from '@/constants/api';
 import { GetMyPlansResponse } from '@/types/apis/plan/GetMyPlans';
 import { axiosInstanceServer } from '@apis/axiosInstanceServer';
 
-export const getMyPlans = async (userId: string) => {
-  try {
-    const { data } = await axiosInstanceServer.get<GetMyPlansResponse>(
-      `/plans/main/${userId}`,
-    );
-    return data;
-  } catch (error) {
-    console.log('Error:', error);
-  }
+export const getMyPlans = async (userId: number) => {
+  const { data } = await axiosInstanceServer.get<GetMyPlansResponse>(
+    DOMAIN.GET_PLANS_MAIN(userId),
+  );
+  return data;
 };
