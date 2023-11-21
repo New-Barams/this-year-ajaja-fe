8c6fccc3a67b12902c734aa46679630bf751ad91
nocheol <qkdl60@gmail.com>
2023-11-21 15:44:04 +0900

    💩 : #94 - 훅 이름 변경

--- src/app/(headerless)/login/hooks/useLoginPage.ts
+++ src/app/(headerless)/login/hooks/useLoginPage.ts
@@ -1,9 +1,9 @@
-import { useLoggedIn } from '@/hooks/useLoggedIn';
+import { useIsLogIn } from '@/hooks/useIsLogIn';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function useLoginPage() {
-  const { isLogin } = useLoggedIn();
+  const { isLogin } = useIsLogIn();
   const router = useRouter();
   useEffect(() => {
     if (isLogin) router.replace('/home');
--- src/hooks/useIsLogIn.ts
+++ src/hooks/useIsLogIn.ts
@@ -1,7 +1,7 @@
 import { authStore } from '@/stores/authStore';
 import { useRecoilValue } from 'recoil';
 
-export const useLoggedIn = () => {
+export const useIsLogIn = () => {
   const auth = useRecoilValue(authStore);
 
   return { isLogin: auth !== null };
