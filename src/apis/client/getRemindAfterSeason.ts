9b04ac2110777d8fecdba1922ba434ec6544c8a0
박민우 <alsd20177@gmail.com>
2023-11-22 21:56:32 +0900

    ✨ : #119 - 리마인드 정보 조회 API 응답 type 재정의

--- src/apis/client/getRemindAfterSeason.ts
+++ src/apis/client/getRemindAfterSeason.ts
@@ -1,10 +1,10 @@
 import { DOMAIN } from '@/constants/api';
-import { RemindData } from '@/types/components/Remind';
+import { GetRemindResponse } from '@/types/apis/remind/getRemind';
 import { axiosInstanceClient } from '../axiosInstanceClient';
 
 export const getRemindAfterSeason = async (
   planId: number,
-): Promise<RemindData> => {
+): Promise<GetRemindResponse> => {
   const { data } = await axiosInstanceClient.get(DOMAIN.GET_REMINDS(planId));
 
   return data;
--- src/apis/client/getRemindSeason.ts
+++ src/apis/client/getRemindSeason.ts
@@ -1,11 +1,17 @@
 import { DOMAIN } from '@/constants/api';
-import { RemindData } from '@/types/components/Remind';
+import { GetRemindResponse } from '@/types/apis/remind/getRemind';
 import { axiosInstanceClient } from '../axiosInstanceClient';
 
-export const getRemindSeason = async (planId: number): Promise<RemindData> => {
+export const getRemindSeason = async (
+  planId: number,
+): Promise<GetRemindResponse> => {
+  console.log('리마인드 정보조회 시즌 API 호출');
   const { data } = await axiosInstanceClient.get(
     DOMAIN.GET_REMINDS_MODIFY(planId),
   );
+
+  console.log(`받아온 data`);
+  console.log(data);
 
   return data;
 };
