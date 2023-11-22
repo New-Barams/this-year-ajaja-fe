00bcb358f14a995a46937c1e30ab5e6ad2b7b622
Ahn Hyesu <linim@naver.com>
2023-11-22 10:20:32 +0900

    ✨ : #97 - 시즌, 비시즌일때 작성 컴포넌트 변경

--- src/app/(header)/home/_components/NewPlan/NewPlan.tsx
+++ src/app/(header)/home/_components/NewPlan/NewPlan.tsx
@@ -1,4 +1,5 @@
 import { Icon } from '@/components';
+import { checkIsSeason } from '@/utils/checkIsSeason';
 import classNames from 'classnames';
 import Link from 'next/link';
 import './index.scss';
@@ -7,10 +8,16 @@ export default function NewPlan() {
   return (
     <Link href={'/create'} className={classNames('new-plan__wrapper')}>
       <div className={classNames('new-plan__wrapper--icon')}>
-        <Icon name="CREATE_NEW_PLAN" size="9xl" color="gray-200" />
+        <Icon
+          name={checkIsSeason() ? 'CREATE_NEW_PLAN' : 'AJAJA'}
+          size="9xl"
+          color="gray-200"
+        />
       </div>
       <p className={classNames('color-origin-gray-200')}>
-        새로운 신년 계획을 생성해보세요!
+        {checkIsSeason()
+          ? '새로운 신년 계획을 생성해보세요!'
+          : '[작성 시즌 종료] 내년에 만나요!'}
       </p>
     </Link>
   );
