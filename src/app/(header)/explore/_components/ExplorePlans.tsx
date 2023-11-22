16a3d25106daa94258475c50003d400726c5e1c3
Ahn Hyesu <linim@naver.com>
2023-11-23 01:09:30 +0900

    ✨ : #89  - 탭 클릭에 따른 data 변경 적용

--- src/app/(header)/explore/_components/ExplorePlans.tsx
+++ src/app/(header)/explore/_components/ExplorePlans.tsx
@@ -1,20 +1,31 @@
 'use client';
 
 import { useAllPlansQuery } from '@/hooks/apis/plans/useAllPlansQuery';
+import { SortType } from '@/types/apis/plan/GetAllPlans';
 import classNames from 'classnames';
+import { useState } from 'react';
 import Plans from './Plans/Plans';
 import Tab from './Tab/Tab';
 import './index.scss';
 
 export default function ExplorePlans() {
+  const [sortCondition, setSortCondition] = useState<SortType>('createdAt');
+  const [isNewYear, setIsNewYear] = useState(true);
   const { allPlans } = useAllPlansQuery({
-    sortCondition: 'ajaja',
-    isNewYear: true,
+    sortCondition,
+    isNewYear,
     pageSize: 5,
   });
+  const handleSort = (condition: SortType) => {
+    setSortCondition(condition);
+  };
+  const handleYear = (isNewYear: boolean) => {
+    setIsNewYear(isNewYear);
+  };
+
   return (
     <div className={classNames('explore-plans__wrapper')}>
-      <Tab />
+      <Tab handleSort={handleSort} handleYear={handleYear} />
       <Plans allPlans={allPlans} />
     </div>
   );
