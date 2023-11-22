cacd7db877ceb6e5bc7af1351af984f77b1f7680
Ahn Hyesu <linim@naver.com>
2023-11-23 01:46:33 +0900

    ✨ : #89 - getMyPlans 서버 변경에 맞춰 변경

--- src/apis/server/getMyPlans.ts
+++ src/apis/server/getMyPlans.ts
@@ -2,9 +2,9 @@ import { DOMAIN } from '@/constants/api';
 import { GetMyPlansResponse } from '@/types/apis/plan/GetMyPlans';
 import { axiosInstanceServer } from '@apis/axiosInstanceServer';
 
-export const getMyPlans = async (userId: number) => {
+export const getMyPlans = async () => {
   const { data } = await axiosInstanceServer.get<GetMyPlansResponse>(
-    DOMAIN.GET_PLANS_MAIN(userId),
+    DOMAIN.GET_PLANS_MAIN,
   );
   return data;
 };
