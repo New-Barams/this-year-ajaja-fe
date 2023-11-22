3426a0eea200df5910e19d5c67862da41332602d
Ahn Hyesu <linim@naver.com>
2023-11-22 17:39:25 +0900

    ✨ : #89 - getAllPlans 관련 API 함수 쿼리 추가

--- src/hooks/apis/plans/useAllPlansQuery.ts
+++ src/hooks/apis/plans/useAllPlansQuery.ts
@@ -1,12 +1,13 @@
 'use client';
 
 import { getAllPlans } from '@/apis/client/getAllPlans';
+import { GetAllPlansRequestQuery } from '@/types/apis/plan/GetAllPlans';
 import { useSuspenseQuery } from '@tanstack/react-query';
 
-export const useAllPlansQuery = () => {
+export const useAllPlansQuery = (query: GetAllPlansRequestQuery) => {
   const { data } = useSuspenseQuery({
-    queryKey: ['all_plans'],
-    queryFn: getAllPlans,
+    queryKey: ['getAllPlans'],
+    queryFn: () => getAllPlans(query),
     staleTime: 10000,
   });
   return { allPlans: data! };
