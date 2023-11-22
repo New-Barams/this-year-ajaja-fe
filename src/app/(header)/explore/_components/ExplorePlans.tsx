dff7a8d4d2973371357516f8383c360bd5054835
Ahn Hyesu <linim@naver.com>
2023-11-22 17:40:51 +0900

    ✨ : #89  - 다른 계획 둘러보기 페이지 실제 사용 API 연결

--- src/app/(header)/explore/_components/ExplorePlans.tsx
+++ src/app/(header)/explore/_components/ExplorePlans.tsx
@@ -1,13 +1,21 @@
+'use client';
+
+import { useAllPlansQuery } from '@/hooks/apis/plans/useAllPlansQuery';
 import classNames from 'classnames';
 import Plans from './Plans/Plans';
 import Tab from './Tab/Tab';
 import './index.scss';
 
 export default function ExplorePlans() {
+  const { allPlans } = useAllPlansQuery({
+    sortCondition: 'ajaja',
+    isNewYear: true,
+    pageSize: 5,
+  });
   return (
     <div className={classNames('explore-plans__wrapper')}>
       <Tab />
-      <Plans />
+      <Plans allPlans={allPlans} />
     </div>
   );
 }
--- src/app/(header)/explore/_components/Plans/Plans.tsx
+++ src/app/(header)/explore/_components/Plans/Plans.tsx
@@ -1,219 +1,221 @@
 'use client';
 
+import { GetAllPlansResponse } from '@/types/apis/plan/GetAllPlans';
 import classNames from 'classnames';
 import Link from 'next/link';
-// import { useAllPlansQuery } from '@/hooks/apis/plans/useAllPlansQuery';
 import Card from '../Card/Card';
 import './index.scss';
 
-export default function Plans() {
-  // const { allPlans } = useAllPlansQuery();
-  // console.log(allPlans.data);
-  const allPlans = {
-    success: true,
-    data: [
-      {
-        id: 0,
-        userId: 1,
-        nickname: '춤추는 고래',
-        title:
-          '올해도 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 ',
-        ajajas: 255,
-        tags: ['태그', '태그태그', '태태태태태태그'],
-        createdAt: '2023-??',
-      },
-      {
-        id: 1,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣zooooooooooo',
-        title:
-          '올해도 아좌좌 아좌좌 아좌좌 김수한무거북이와두루미암튼모르겠는데긴이름름이름이다',
-        ajajas: 0,
-        tags: [
-          '태그',
-          '태그태그',
-          '태태태태태태그',
-          '태태태태태태그',
-          '태태태태그태태태태그',
-        ],
-        createdAt: '2023-??',
-      },
-      {
-        id: 2,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣',
-        title: '올해도 아좌좌 아좌좌',
-        ajajas: 1000,
-        tags: [
-          '태그',
-          '태그태그',
-          '태태태태태태그',
-          '태태태태태태그',
-          '태태태태그태태태태그',
-        ],
-        createdAt: '2023-??',
-      },
-      {
-        id: 3,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣',
-        title: '올해도 아좌좌 아좌좌',
-        ajajas: 2231,
-        tags: ['태그', '태그', '태태', '태태', '태태'],
-        createdAt: '2023-??',
-      },
-      {
-        id: 0,
-        userId: 1,
-        nickname: '춤추는 고래',
-        title:
-          '올해도 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 ',
-        ajajas: 255,
-        tags: ['태그', '태그태그', '태태태태태태그'],
-        createdAt: '2023-??',
-      },
-      {
-        id: 1,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣zooooooooooo',
-        title:
-          '올해도 아좌좌 아좌좌 아좌좌 김수한무거북이와두루미암튼모르겠는데긴이름름이름이다',
-        ajajas: 0,
-        tags: [
-          '태그',
-          '태그태그',
-          '태태태태태태그',
-          '태태태태태태그',
-          '태태태태그태태태태그',
-        ],
-        createdAt: '2023-??',
-      },
-      {
-        id: 2,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣',
-        title: '올해도 아좌좌 아좌좌',
-        ajajas: 1000,
-        tags: [
-          '태그',
-          '태그태그',
-          '태태태태태태그',
-          '태태태태태태그',
-          '태태태태그태태태태그',
-        ],
-        createdAt: '2023-??',
-      },
-      {
-        id: 3,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣',
-        title: '올해도 아좌좌 아좌좌',
-        ajajas: 2231,
-        tags: ['태그', '태그', '태태', '태태', '태태'],
-        createdAt: '2023-??',
-      },
-      {
-        id: 0,
-        userId: 1,
-        nickname: '춤추는 고래',
-        title:
-          '올해도 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 ',
-        ajajas: 255,
-        tags: ['태그', '태그태그', '태태태태태태그'],
-        createdAt: '2023-??',
-      },
-      {
-        id: 1,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣zooooooooooo',
-        title:
-          '올해도 아좌좌 아좌좌 아좌좌 김수한무거북이와두루미암튼모르겠는데긴이름름이름이다',
-        ajajas: 0,
-        tags: [
-          '태그',
-          '태그태그',
-          '태태태태태태그',
-          '태태태태태태그',
-          '태태태태그태태태태그',
-        ],
-        createdAt: '2023-??',
-      },
-      {
-        id: 2,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣',
-        title: '올해도 아좌좌 아좌좌',
-        ajajas: 1000,
-        tags: [
-          '태그',
-          '태그태그',
-          '태태태태태태그',
-          '태태태태태태그',
-          '태태태태그태태태태그',
-        ],
-        createdAt: '2023-??',
-      },
-      {
-        id: 3,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣',
-        title: '올해도 아좌좌 아좌좌',
-        ajajas: 2231,
-        tags: ['태그', '태그', '태태', '태태', '태태'],
-        createdAt: '2023-??',
-      },
-      {
-        id: 0,
-        userId: 1,
-        nickname: '춤추는 고래',
-        title:
-          '올해도 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 ',
-        ajajas: 255,
-        tags: ['태그', '태그태그', '태태태태태태그'],
-        createdAt: '2023-??',
-      },
-      {
-        id: 1,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣zooooooooooo',
-        title:
-          '올해도 아좌좌 아좌좌 아좌좌 김수한무거북이와두루미암튼모르겠는데긴이름름이름이다',
-        ajajas: 0,
-        tags: [
-          '태그',
-          '태그태그',
-          '태태태태태태그',
-          '태태태태태태그',
-          '태태태태그태태태태그',
-        ],
-        createdAt: '2023-??',
-      },
-      {
-        id: 2,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣',
-        title: '올해도 아좌좌 아좌좌',
-        ajajas: 1000,
-        tags: [
-          '태그',
-          '태그태그',
-          '태태태태태태그',
-          '태태태태태태그',
-          '태태태태그태태태태그',
-        ],
-        createdAt: '2023-??',
-      },
-      {
-        id: 3,
-        userId: 2,
-        nickname: '윈드밀하는 미어캣',
-        title: '올해도 아좌좌 아좌좌',
-        ajajas: 2231,
-        tags: ['태그', '태그', '태태', '태태', '태태'],
-        createdAt: '2023-??',
-      },
-    ],
-  };
+type PlansProps = {
+  allPlans: GetAllPlansResponse;
+};
+
+export default function Plans({ allPlans }: PlansProps) {
+  // const allPlans = {
+  //   success: true,
+  //   data: [
+  //     {
+  //       id: 0,
+  //       userId: 1,
+  //       nickname: '춤추는 고래',
+  //       title:
+  //         '올해도 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 ',
+  //       ajajas: 255,
+  //       tags: ['태그', '태그태그', '태태태태태태그'],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 1,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣zooooooooooo',
+  //       title:
+  //         '올해도 아좌좌 아좌좌 아좌좌 김수한무거북이와두루미암튼모르겠는데긴이름름이름이다',
+  //       ajajas: 0,
+  //       tags: [
+  //         '태그',
+  //         '태그태그',
+  //         '태태태태태태그',
+  //         '태태태태태태그',
+  //         '태태태태그태태태태그',
+  //       ],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 2,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣',
+  //       title: '올해도 아좌좌 아좌좌',
+  //       ajajas: 1000,
+  //       tags: [
+  //         '태그',
+  //         '태그태그',
+  //         '태태태태태태그',
+  //         '태태태태태태그',
+  //         '태태태태그태태태태그',
+  //       ],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 3,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣',
+  //       title: '올해도 아좌좌 아좌좌',
+  //       ajajas: 2231,
+  //       tags: ['태그', '태그', '태태', '태태', '태태'],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 0,
+  //       userId: 1,
+  //       nickname: '춤추는 고래',
+  //       title:
+  //         '올해도 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 ',
+  //       ajajas: 255,
+  //       tags: ['태그', '태그태그', '태태태태태태그'],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 1,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣zooooooooooo',
+  //       title:
+  //         '올해도 아좌좌 아좌좌 아좌좌 김수한무거북이와두루미암튼모르겠는데긴이름름이름이다',
+  //       ajajas: 0,
+  //       tags: [
+  //         '태그',
+  //         '태그태그',
+  //         '태태태태태태그',
+  //         '태태태태태태그',
+  //         '태태태태그태태태태그',
+  //       ],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 2,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣',
+  //       title: '올해도 아좌좌 아좌좌',
+  //       ajajas: 1000,
+  //       tags: [
+  //         '태그',
+  //         '태그태그',
+  //         '태태태태태태그',
+  //         '태태태태태태그',
+  //         '태태태태그태태태태그',
+  //       ],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 3,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣',
+  //       title: '올해도 아좌좌 아좌좌',
+  //       ajajas: 2231,
+  //       tags: ['태그', '태그', '태태', '태태', '태태'],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 0,
+  //       userId: 1,
+  //       nickname: '춤추는 고래',
+  //       title:
+  //         '올해도 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 ',
+  //       ajajas: 255,
+  //       tags: ['태그', '태그태그', '태태태태태태그'],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 1,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣zooooooooooo',
+  //       title:
+  //         '올해도 아좌좌 아좌좌 아좌좌 김수한무거북이와두루미암튼모르겠는데긴이름름이름이다',
+  //       ajajas: 0,
+  //       tags: [
+  //         '태그',
+  //         '태그태그',
+  //         '태태태태태태그',
+  //         '태태태태태태그',
+  //         '태태태태그태태태태그',
+  //       ],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 2,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣',
+  //       title: '올해도 아좌좌 아좌좌',
+  //       ajajas: 1000,
+  //       tags: [
+  //         '태그',
+  //         '태그태그',
+  //         '태태태태태태그',
+  //         '태태태태태태그',
+  //         '태태태태그태태태태그',
+  //       ],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 3,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣',
+  //       title: '올해도 아좌좌 아좌좌',
+  //       ajajas: 2231,
+  //       tags: ['태그', '태그', '태태', '태태', '태태'],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 0,
+  //       userId: 1,
+  //       nickname: '춤추는 고래',
+  //       title:
+  //         '올해도 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 아좌좌 ',
+  //       ajajas: 255,
+  //       tags: ['태그', '태그태그', '태태태태태태그'],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 1,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣zooooooooooo',
+  //       title:
+  //         '올해도 아좌좌 아좌좌 아좌좌 김수한무거북이와두루미암튼모르겠는데긴이름름이름이다',
+  //       ajajas: 0,
+  //       tags: [
+  //         '태그',
+  //         '태그태그',
+  //         '태태태태태태그',
+  //         '태태태태태태그',
+  //         '태태태태그태태태태그',
+  //       ],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 2,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣',
+  //       title: '올해도 아좌좌 아좌좌',
+  //       ajajas: 1000,
+  //       tags: [
+  //         '태그',
+  //         '태그태그',
+  //         '태태태태태태그',
+  //         '태태태태태태그',
+  //         '태태태태그태태태태그',
+  //       ],
+  //       createdAt: '2023-??',
+  //     },
+  //     {
+  //       id: 3,
+  //       userId: 2,
+  //       nickname: '윈드밀하는 미어캣',
+  //       title: '올해도 아좌좌 아좌좌',
+  //       ajajas: 2231,
+  //       tags: ['태그', '태그', '태태', '태태', '태태'],
+  //       createdAt: '2023-??',
+  //     },
+  //   ],
+  // };
   return (
     <div className={classNames('plans__wrapper')}>
       {allPlans.data.map((plan, index) => {
