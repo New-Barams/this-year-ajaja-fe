3dbc68340f8dbe9b022406156494ad4797eec20f
nocheol <qkdl60@gmail.com>
2023-11-23 21:32:44 +0900

    ♻️ : #133 - 로그인api 변경

--- src/apis/client/getTokenWithCode.ts
+++ src/apis/client/getTokenWithCode.ts
@@ -1,11 +1,18 @@
 import { DOMAIN } from '@/constants/api';
-import { getTokenWithCodeResponse } from '@/types/apis/login/getTokenWithCoin';
+import {
+  getTokenWithCodeRequestBody,
+  getTokenWithCodeResponse,
+} from '@/types/apis/login/getTokenWithCoin';
 import { axiosInstanceClient } from '../axiosInstanceClient';
 
 export const getTokenWithCode = async (code: string) => {
+  const requestBody: getTokenWithCodeRequestBody = {
+    authorizationCode: code,
+    redirectUrl: `${process.env.NEXT_PUBLIC_REDIRECT_URL}`,
+  };
   const { data } = await axiosInstanceClient.post<getTokenWithCodeResponse>(
-    DOMAIN.POST_LOGIN(code),
-    null,
+    DOMAIN.POST_LOGIN,
+    requestBody,
     {
       authorization: false,
     },
--- src/constants/api.ts
+++ src/constants/api.ts
@@ -15,7 +15,7 @@ export const DOMAIN = {
   GET_FEEDBACKS_EACH: (planId: number) => `/mock/${planId}/feedbacks`,
 
   POST_REISSUE: '/mock/reissue',
-  POST_LOGIN: (code: string) => `/mock/login?code=${code}`,
+  POST_LOGIN: `/mock/login`,
 
   GET_PLANS_REMINDS: (planId: number) => `/mock/plans/${planId}/reminds`,
   PUT_PLANS_REMINDS: (planId: number) => `/mock/plans/${planId}/reminds`,
