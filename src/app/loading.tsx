c69ffb9bbe486734e321c350be125b3c1fd69ad8
박민우 <alsd20177@gmail.com>
2023-11-23 22:16:00 +0900

    ✨ : #122 - loading 컴포넌트 react-spinners 라이브러리 사용해서 재정의

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,11 @@
+import { COLOR } from '@/constants';
+import { FadeLoader } from 'react-spinners';
+import './loading.scss';
+
 export default function Loading() {
-  return <h1 style={{ margin: '200px' }}>로딩 중입니다 </h1>;
+  return (
+    <h1 className="loading__wrapper">
+      <FadeLoader color={COLOR.PRIMARY} speedMultiplier={1.3} />
+    </h1>
+  );
 }
