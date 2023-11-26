de3bf6dee979c2832cee3d7578b23a580d89f18d
nocheol <qkdl60@gmail.com>
2023-11-26 15:23:08 +0900

    ♻️ : #150 - 로그인 url 변경

--- src/constants/login.ts
+++ src/constants/login.ts
@@ -1,2 +1,2 @@
-export const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URL}&response_type=code`;
+export const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URL}?way=login&response_type=code`;
 export const UN_AUTH_URL = '/explore';
