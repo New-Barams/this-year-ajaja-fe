b2bbd979addc0ac2f2f32028789110f1d4fcc7da
Ahn Hyesu <linim@naver.com>
2023-11-28 01:07:56 +0900

    ✨ : #143 - 무한 스크롤 구현

--- src/app/(header)/explore/_components/ExplorePlans.tsx
+++ src/app/(header)/explore/_components/ExplorePlans.tsx
@@ -1,39 +1,58 @@
 'use client';
 
+import { ToTopFloatingButton } from '@/components';
+import { COLOR } from '@/constants';
 import { useAllPlansQuery } from '@/hooks/apis/plans/useAllPlansQuery';
 import { SortType } from '@/types/apis/plan/GetAllPlans';
 import classNames from 'classnames';
-import { useMemo, useState } from 'react';
+import { useMemo, useRef, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
+import { FadeLoader } from 'react-spinners';
 import Plans from './Plans/Plans';
 import Tab from './Tab/Tab';
 import './index.scss';
 
 export default function ExplorePlans() {
-  const [sortCondition, setSortCondition] = useState<SortType>('createdAt');
-  const [isNewYear, setIsNewYear] = useState(true);
-  const { loadedPlans, fetchNextPage, hasNextPage, isLoading, isError } =
-    useAllPlansQuery({
-      sortCondition,
-      isNewYear,
-      pageSize: 1,
-    });
+  const scrollParentRef = useRef(null);
+  const [sort, setSort] = useState<SortType>('latest');
+  const [current, setCurrent] = useState(true);
+  const { loadedPlans, fetchNextPage, hasNextPage } = useAllPlansQuery({
+    sort,
+    current,
+  });
   const flatLoadedPlans = useMemo(() => loadedPlans.flat(), [loadedPlans]);
-  console.log('loadedPlans:', loadedPlans);
-  console.log('flatLoadedPlans:', flatLoadedPlans);
+  // console.log('loadedPlans:', loadedPlans);
+  // console.log('flatLoadedPlans:', flatLoadedPlans);
   const handleSort = (condition: SortType) => {
-    setSortCondition(condition);
+    setSort(condition);
   };
   const handleYear = (isNewYear: boolean) => {
-    setIsNewYear(isNewYear);
+    setCurrent(isNewYear);
   };
-  console.log('hasNextPage:', hasNextPage);
-  console.log('isLoading:', isLoading);
-  console.log('isError:', isError);
-  fetchNextPage();
   return (
-    <div className={classNames('explore-plans__wrapper')}>
-      <Tab handleSort={handleSort} handleYear={handleYear} />
-      <Plans flatLoadedPlans={flatLoadedPlans} />
+    <div ref={scrollParentRef} className={classNames('explore-plans')}>
+      <div className={classNames('explore-plans__wrapper')}>
+        <Tab handleSort={handleSort} handleYear={handleYear} />
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={() => fetchNextPage()}
+          hasMore={hasNextPage}
+          loader={
+            <FadeLoader
+              key="loader"
+              color={COLOR.PRIMARY}
+              speedMultiplier={1}
+              className={classNames('explore-plans__loader')}
+            />
+          }
+          useWindow={false}
+          getScrollParent={() => {
+            return scrollParentRef.current;
+          }}>
+          <Plans flatLoadedPlans={flatLoadedPlans} />
+        </InfiniteScroll>
+      </div>
+      <ToTopFloatingButton />
     </div>
   );
 }
