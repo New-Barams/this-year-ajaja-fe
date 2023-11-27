6197b3b896e215ebb1fc2f1ceccc6f581598de26
Ahn Hyesu <linim@naver.com>
2023-11-27 12:29:01 +0900

    ✨ : #143 - useAllPlansQuery 무한 스크롤 기능으로 변경

--- src/hooks/apis/plans/useAllPlansQuery.ts
+++ src/hooks/apis/plans/useAllPlansQuery.ts
@@ -2,13 +2,40 @@
 
 import { getAllPlans } from '@/apis/client/getAllPlans';
 import { GetAllPlansRequestQuery } from '@/types/apis/plan/GetAllPlans';
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
 export const useAllPlansQuery = (query: GetAllPlansRequestQuery) => {
-  const { data } = useSuspenseQuery({
-    queryKey: ['getAllPlans', query.sortCondition, query.isNewYear],
-    queryFn: () => getAllPlans(query),
-    staleTime: 10000,
-  });
-  return { allPlans: data! };
+  const { data, fetchNextPage, hasNextPage, isLoading, isError } =
+    useInfiniteQuery({
+      queryKey: ['getAllPlans', query.sortCondition, query.isNewYear],
+      queryFn: async ({ pageParam }) => {
+        let params = {
+          sortCondition: query.sortCondition,
+          isNewYear: query.isNewYear,
+          pageSize: query.pageSize,
+        };
+
+        if (pageParam) {
+          params = { ...params, ...pageParam };
+        }
+        const result = await getAllPlans(params);
+        return result?.data;
+      },
+      initialPageParam: {},
+      getNextPageParam: (lastPage) => {
+        const lastItem = lastPage[lastPage.length - 1];
+        return lastItem
+          ? { cursorCreatedAt: lastItem.createdAt, cursorId: lastItem.id }
+          : undefined;
+      },
+      staleTime: 10000,
+    });
+  console.log('Data:', data?.pages);
+  return {
+    loadedPlans: data?.pages || [],
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isError,
+  };
 };
