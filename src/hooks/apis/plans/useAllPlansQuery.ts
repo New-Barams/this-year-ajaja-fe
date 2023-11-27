00f0b04d6f77c2ebd2438887d38b91e2977a1de2
Ahn Hyesu <linim@naver.com>
2023-11-28 01:29:24 +0900

    ♻️ : #169 - getAllPlansQuery 쿼리키 상수화

--- src/hooks/apis/plans/useAllPlansQuery.ts
+++ src/hooks/apis/plans/useAllPlansQuery.ts
@@ -1,12 +1,13 @@
 'use client';
 
 import { getAllPlans } from '@/apis/client/getAllPlans';
+import { QUERY_KEY } from '@/constants/queryKey';
 import { GetAllPlansRequestQuery } from '@/types/apis/plan/GetAllPlans';
 import { useSuspenseQuery } from '@tanstack/react-query';
 
 export const useAllPlansQuery = (query: GetAllPlansRequestQuery) => {
   const { data } = useSuspenseQuery({
-    queryKey: ['getAllPlans', query.sortCondition, query.isNewYear],
+    queryKey: [QUERY_KEY.ALL_PLANS, query.sortCondition, query.isNewYear],
     queryFn: () => getAllPlans(query),
     staleTime: 10000,
   });
