7d214b51c70a3a44aede31f06540879538a2df14
박민우 <alsd20177@gmail.com>
2023-11-27 22:12:19 +0900

    ✨ : #165 - 피드백 반영 api mutation 쿼리 무효화 로직 추가 및 ReadOnlyRemindItem 컴포넌트 prop으로 planId 추가

--- src/hooks/apis/feedback/usePostFeedbackMutation.ts
+++ src/hooks/apis/feedback/usePostFeedbackMutation.ts
@@ -1,8 +1,19 @@
 import { postFeedback } from '@/apis/client/postFeedback';
-import { useMutation } from '@tanstack/react-query';
+import { QUERY_KEY } from '@/constants/queryKey';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-export const usePostFeedbackMutation = () => {
+export const usePostFeedbackMutation = (planId: number) => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: postFeedback,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEY.MY_PLANS],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [{ planId: planId }, QUERY_KEY.REMIND],
+      });
+    },
   });
 };
