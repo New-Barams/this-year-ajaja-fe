92252269c9f982015a8292bc8a9113b20ead1320
박민우 <alsd20177@gmail.com>
2023-11-27 22:00:33 +0900

    ✨ : #165 - 공개여부, 아좌좌알림, 리마인드알림 toggle mutation 무효화 로직 추가

--- src/hooks/apis/useDeletePlanMutation.ts
+++ src/hooks/apis/useDeletePlanMutation.ts
@@ -1,4 +1,5 @@
 import { deletePlan } from '@/apis/client/deletePlan';
+import { QUERY_KEY } from '@/constants/queryKey';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useDeletePlanMutation = () => {
