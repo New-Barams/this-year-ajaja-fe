3f0f7331f7fd6da90817add8577e3cf0c754b6de
박민우 <alsd20177@gmail.com>
2023-11-28 15:14:00 +0900

    ✨ : #183 - queryClient suspense 옵션 삭제

--- src/provider/TanstackQueryProvider.tsx
+++ src/provider/TanstackQueryProvider.tsx
@@ -4,13 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      suspense: true,
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 export default function TanstackQueryProvider({
   children,
