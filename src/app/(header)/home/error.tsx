bd06394d9464edf7f0aed8702a2558c1aed3a9f3
Ahn Hyesu <linim@naver.com>
2023-11-28 16:14:15 +0900

    ✨ : #186 -  내 계획 페이지 에러처리

---
+++ src/app/(header)/home/error.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { BasicError } from '@/components';
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return <BasicError reset={reset} />;
+}
