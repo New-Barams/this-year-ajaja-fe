36e6aae212f77c30b1a0cc13eeb77473eaeb3891
Ahn Hyesu <linim@naver.com>
2023-12-17 22:01:37 +0900

    ✨ : #261 - 네비게이션 페이지마다 네비게이션바  색상 수정

--- src/app/_components/Navigation/Navigation.tsx
+++ src/app/_components/Navigation/Navigation.tsx
@@ -5,13 +5,14 @@ import { checkIsSeason } from '@/utils/checkIsSeason';
 import classNames from 'classnames';
 import { hasCookie } from 'cookies-next';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 import './index.scss';
 
 export default function Navigation({ hasAuth }: { hasAuth: boolean }) {
+  const pathName = usePathname();
   const [isLogin, setIsLogin] = useState(hasAuth);
 
-  // console.log(useRouter());
   if (!hasCookie('auth')) {
     setTimeout(() => {
       setIsLogin(hasCookie('auth'));
@@ -22,28 +23,62 @@ export default function Navigation({ hasAuth }: { hasAuth: boolean }) {
     <div className={classNames('navigation')}>
       <Link
         href="/home"
-        className={classNames('navigation-icon', 'color-origin-text-300')}>
-        <Icon name="HOME" isFilled={true} size="xl" />
+        className={classNames('navigation-icon', {
+          'color-origin-primary': pathName === '/home',
+          'color-origin-text-300': pathName !== '/home',
+        })}>
+        <Icon
+          name="HOME"
+          isFilled={true}
+          size="xl"
+          color={pathName === '/home' ? 'primary' : 'text-300'}
+        />
         <p className={classNames('font-size-xs')}>홈</p>
       </Link>
       <Link
         href="/create"
-        className={classNames('navigation-icon', 'color-origin-text-300')}>
-        <Icon name="CREATE_NEW_PLAN" isFilled={true} size="xl" />
+        className={classNames('navigation-icon', {
+          'color-origin-primary': pathName === '/create',
+          'color-origin-text-300': pathName !== '/create',
+        })}>
+        <Icon
+          name="CREATE_NEW_PLAN"
+          isFilled={true}
+          size="xl"
+          color={pathName === '/create' ? 'primary' : 'text-300'}
+        />
         <p className={classNames('font-size-xs')}>
           {checkIsSeason() ? '계획 작성' : '피드백하기'}
         </p>
       </Link>
       <Link
         href="/explore"
-        className={classNames('navigation-icon', 'color-origin-text-300')}>
-        <Icon name="OTHER_PLAN" isFilled={true} size="xl" />
+        className={classNames('navigation-icon', {
+          'color-origin-primary': pathName === '/explore',
+          'color-origin-text-300': pathName !== '/explore',
+        })}>
+        <Icon
+          name="OTHER_PLAN"
+          isFilled={true}
+          size="xl"
+          color={pathName === '/explore' ? 'primary' : 'text-300'}
+        />
         <p className={classNames('font-size-xs')}>둘러보기</p>
       </Link>
       <Link
         href={isLogin ? '/my' : '/login'}
-        className={classNames('navigation-icon', 'color-origin-text-300')}>
-        <Icon name={isLogin ? 'PROFILE' : 'LOGIN'} isFilled={true} size="xl" />
+        className={classNames('navigation-icon', {
+          'color-origin-primary': pathName === '/my' || pathName === '/login',
+          'color-origin-text-300': pathName !== '/my' && pathName !== '/login',
+        })}>
+        <Icon
+          name={isLogin ? 'PROFILE' : 'LOGIN'}
+          isFilled={true}
+          size="xl"
+          color={
+            pathName === '/my' || pathName === '/login' ? 'primary' : 'text-300'
+          }
+        />
         <p className={classNames('font-size-xs')}>
           {isLogin ? '마이페이지' : '로그인하기'}
         </p>
