23764bf034343949aae1881324584b7f1dfc30fb
Ahn Hyesu <linim@naver.com>
2023-12-18 23:37:42 +0900

    ✨ : #262 - 홈 계획 생성 1차 완료 1. 시즌일 때 계획 생성 가능, 불가능 완료 2. 비시즌일 때 계획 생성 불가능 완료

--- src/app/(header)/home/_components/MyPlan.tsx
+++ src/app/(header)/home/_components/MyPlan.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import { Dropdown, Icon, Modal, ModalVerification, Tag } from '@/components';
-import { QUERY_KEY } from '@/constants/queryKey';
-import { useGetUserInformationQuery } from '@/hooks/apis/useGetUserInformationQuery';
+import { Dropdown } from '@/components';
+import { planIcons } from '@/constants/planIcons';
 import { GetMyPlansResponse } from '@/types/apis/plan/GetMyPlans';
-import { useQueryClient } from '@tanstack/react-query';
 import classNames from 'classnames';
 import { useEffect, useState } from 'react';
 import NewPlan from './NewPlan/NewPlan';
@@ -16,66 +14,46 @@ type MyPlanProps = {
 };
 
 export default function MyPlan({ myPlans }: MyPlanProps) {
-  const maxLength = 4;
-  const queryClient = useQueryClient();
+  const maxLength = 2;
   const { data: myPlansData } = myPlans;
   const yearList = myPlansData.map((x) => x.year);
   const [period, setPeriod] = useState(yearList[0]);
   const [yearData, setYearData] = useState(myPlansData[0]);
-  const { userInformation } = useGetUserInformationQuery();
-  const email_isVerified = userInformation?.emailVerified;
-  const [isOpenEmailModal, setIsOpenEmailModal] = useState(false);
   const PERIOD_OPTIONS = yearList.map((x) => {
     return { value: x, name: `${x}년 계획` };
   });
 
-  const handleOpenEmailVerificationModal = () => {
-    if (!email_isVerified) {
-      setIsOpenEmailModal(true);
-    }
-  };
-  const handleCloseEmailVerificationModal = () => {
-    setIsOpenEmailModal(false);
-  };
-  const handleSetVerifiedEmail = () => {
-    Promise.all([
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.MY_PLANS] }),
-      queryClient.invalidateQueries({
-        queryKey: [QUERY_KEY.USER_INFORMATION],
-      }),
-    ]);
-  };
-
   useEffect(() => {
     setYearData(myPlansData.find((x) => x.year === period)!);
   }, [period, myPlansData]);
-
   return (
     <>
-      <div className={classNames(`home__wrapper-dropdown`)}>
-        <Dropdown
-          dropdownId="homePageDropdown"
-          options={PERIOD_OPTIONS}
-          selectedValue={period}
-          setSelectedValue={setPeriod}
-        />
-      </div>
-      <div
-        className={classNames(
-          `home__wrapper--year`,
-          `font-size-3xl`,
-          `color-origin-gray-300`,
-        )}>
-        {period}년 나의 계획은?
-      </div>
-      <ProgressBar percent={myPlansData[0].totalAchieveRate} />
-      <div
-        className={classNames(
-          `home__wrapper--total`,
-          `font-size-base`,
-          `color-origin-gray-200`,
-        )}>
-        전체 달성률 : {myPlansData[0].totalAchieveRate}%
+      <div className={classNames('home__header')}>
+        <div className={classNames(`home__header-dropdown`)}>
+          <Dropdown
+            dropdownId="homePageDropdown"
+            options={PERIOD_OPTIONS}
+            selectedValue={period}
+            setSelectedValue={setPeriod}
+          />
+        </div>
+        <div
+          className={classNames(
+            `home__header--year`,
+            `font-size-lg`,
+            `color-origin-text-100`,
+          )}>
+          {period}년 나의 계획은?
+        </div>
+        <ProgressBar percent={yearData.totalAchieveRate} />
+        <div
+          className={classNames(
+            `home__header--total`,
+            `font-size-base`,
+            `color-origin-text-100`,
+          )}>
+          전체 달성률 : {yearData.totalAchieveRate}%
+        </div>
       </div>
       <div className={classNames('home__plans')}>
         {yearData.getPlanList.map((plan, index) => {
@@ -85,50 +63,15 @@ export default function MyPlan({ myPlans }: MyPlanProps) {
               title={plan.title}
               planId={plan.planId}
               achieveRate={plan.achieveRate}
-              icon={plan.icon}
+              photoUrl={`/animal/${planIcons[plan.icon]}.png`}
             />
           );
         })}
-        {Array.from(
-          { length: maxLength - yearData.getPlanList.length },
-          (_, i) => {
-            return (
-              <NewPlan
-                key={i}
-                email_isVerified={email_isVerified}
-                handleOpenEmailVerificationModal={
-                  handleOpenEmailVerificationModal
-                }
-              />
-            );
-          },
-        )}
+        <NewPlan
+          maxLength={maxLength}
+          currentLength={yearData.getPlanList.length}
+        />
       </div>
-      {email_isVerified ? (
-        <p className={classNames('home-email-isVerified')}>
-          현재{' '}
-          <Tag color="green-300" classNameList={['home-email--tag']}>
-            이메일
-          </Tag>
-          을 통해서 리마인드를 받고 있어요
-        </p>
-      ) : (
-        <p className={classNames('home-email-isVerified')}>
-          <Icon name="WARNING" size="xl" />
-          현재 인증된 이메일이 없습니다.
-        </p>
-      )}
-      <p className={classNames('font-size-sm', 'color-origin-gray-200')}>
-        마이페이지에서 리마인드 방식을 변경할 수 있어요!
-      </p>
-      {isOpenEmailModal && (
-        <Modal>
-          <ModalVerification
-            handleCloseModal={handleCloseEmailVerificationModal}
-            setVerifiedEmail={handleSetVerifiedEmail}
-          />
-        </Modal>
-      )}
     </>
   );
 }
