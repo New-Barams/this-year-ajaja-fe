974b6d48e774be78a23479eebd30f42ebb7ebd3b
박민우 <alsd20177@gmail.com>
2023-12-20 17:49:02 +0900

    ✨ : #282 - ReadOnlyRemindItem 컴포넌트 불필요한 로직 제거 및 디자인 구현

--- src/app/(header)/reminds/page.tsx
+++ src/app/(header)/reminds/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { IconSwitchButton } from '@/components';
 import classNames from 'classnames';
 import React from 'react';
 import './index.scss';
@@ -7,7 +8,27 @@ import './index.scss';
 export default function RemindPage() {
   return (
     <div className={classNames(['remind-page'])}>
-      <div className={classNames(['remind-page__title'])}>리마인드</div>
+      <div className={classNames(['remind-page__title', 'font-size-xl'])}>
+        리마인드
+      </div>
+
+      <p>수정</p>
+
+      <ul></ul>
+
+      <p>
+        <span>아침(09시)</span>에 리마인드를 받고 있어요!
+      </p>
+
+      <IconSwitchButton
+        onIconName="NOTIFICATION_ON"
+        offIconName="NOTIFICATION_OFF"
+        onClick={() => {
+          console.log('hello');
+        }}
+        isActive={false}
+      />
+      <p>리마인드 알림 활성화</p>
     </div>
   );
 }
