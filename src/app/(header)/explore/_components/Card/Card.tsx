7d4c27369d1927f1e67deafa66ceae1875b0de19
Ahn Hyesu <linim@naver.com>
2023-12-20 15:50:31 +0900

    💄  : #280 - 카드 컴포넌트 디자인 수정

--- src/app/(header)/explore/_components/Card/Card.tsx
+++ src/app/(header)/explore/_components/Card/Card.tsx
@@ -1,73 +1,62 @@
 import { AjajaButton, Tag } from '@/components';
-import { Color } from '@/types';
 import { CardPlans } from '@/types/apis/plan/GetAllPlans';
 import classNames from 'classnames';
+import Image from 'next/image';
 import './index.scss';
-
-const color: Color[] = [
-  'primary',
-  'orange-300',
-  'green-300',
-  'blue-300',
-  'purple-300',
-];
 
 type CardProps = {
   plan: CardPlans;
 };
 export default function Card({ plan }: CardProps) {
   return (
-    <div className={classNames('card__wrapper')}>
+    <div
+      className={classNames(
+        'card__wrapper',
+        'border-origin-secondary',
+        'border-round',
+      )}>
+      <Image
+        src="/animal/alpaca.png"
+        width={60}
+        height={60}
+        alt="animal icon"
+        className={classNames('card__wrapper--image')}
+      />
       <div
         className={classNames(
-          'card__background',
-          'border-round',
-          'background-origin-orange-300',
-        )}></div>
-      <div
-        className={classNames(
-          'card__contents',
-          'border-round',
-          'border-origin-orange-300',
-          'background-origin-white-300',
+          'card__contents-wrapper',
+          'color-origin-text-100',
         )}>
-        <div className={classNames('card__contents-wrapper')}>
-          <p
-            className={classNames(
-              'card__contents--title',
-              'font-size-lg',
-              'color-origin-gray-300',
-            )}>
-            {plan.title}
-          </p>
-          <p
-            className={classNames(
-              'card__contents--nickname',
-              'font-size-sm',
-              'color-origin-gray-200',
-            )}>
-            {plan.nickname}님의 계획 • {plan.createdAt.slice(0, 4)}년 작성
-          </p>
-          <div className={classNames('card__contents--tags', 'font-size-xs')}>
-            {plan.tags.map((tag, index) => {
-              return (
-                <Tag
-                  key={index}
-                  color={color[index]}
-                  style={{ padding: '0.2rem 0.5rem' }}>
-                  {tag}
-                </Tag>
-              );
-            })}
-          </div>
-          <AjajaButton
-            planId={plan.id}
-            ajajaCount={plan.ajajas}
-            isFilled={true}
-            classNameList={['card__contents--ajaja']}
-            disabled
-          />
+        <p className={classNames('card__contents--title', 'font-size-md')}>
+          {plan.title}
+        </p>
+        <p className={classNames('card__contents--nickname', 'font-size-xs')}>
+          {plan.nickname}님의 계획 • {plan.createdAt.slice(0, 4)}년 작성
+        </p>
+        <AjajaButton
+          planId={plan.id}
+          ajajaCount={plan.ajajas}
+          isFilled={true}
+          disabled
+        />
+        <div className={classNames('card__contents--tags')}>
+          {plan.tags.length ? (
+            <>
+              {plan.tags.map((tag, index) => {
+                return (
+                  <Tag key={index} style={{ padding: ' 0 0.25rem' }}>
+                    {tag}
+                  </Tag>
+                );
+              })}
+            </>
+          ) : (
+            <p className={classNames('font-size-sm', 'color-origin-secondary')}>
+              태그가 없습니다.
+            </p>
+          )}
         </div>
+        {/* </div> */}
       </div>
     </div>
   );
