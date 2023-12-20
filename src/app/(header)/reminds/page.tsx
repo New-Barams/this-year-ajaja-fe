acc082117030409199f35f6c376c05c3cafe5c87
박민우 <alsd20177@gmail.com>
2023-12-20 16:19:47 +0900

    ✨ : #282 - remind 페이지 기본 page, error, css 파일 생성

---
+++ src/app/(header)/reminds/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import classNames from 'classnames';
+import React from 'react';
+import './index.scss';
+
+export default function RemindPage() {
+  return (
+    <div className={classNames(['remind-page'])}>
+      <div className={classNames(['remind-page__title'])}>리마인드</div>
+    </div>
+  );
+}
