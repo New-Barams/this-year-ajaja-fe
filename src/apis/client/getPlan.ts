f616035c21c7a0ab04eda5a439885f1a1c5bb63a
nocheol <qkdl60@gmail.com>
2023-12-29 00:58:04 +0900

    ♻️ : #322 - getplan 관련 api 변경

--- src/apis/client/getPlan.ts
+++ src/apis/client/getPlan.ts
@@ -2,7 +2,7 @@ import { axiosInstanceClient } from '@/apis/axiosInstanceClient';
 import { DOMAIN } from '@/constants/api';
 import { GetPlanResponse } from '@/types/apis/plan/GetPlan';
 
-export const getPlan = async (id: number, isLogin: boolean = false) => {
+export const getPlan = async (id: number, isLogin: boolean) => {
   const { data } = await axiosInstanceClient.get<GetPlanResponse>(
     DOMAIN.GET_PLANS(id),
     { authorization: isLogin },
