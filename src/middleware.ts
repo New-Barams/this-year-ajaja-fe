9ccfae8867d9c2c8b723e976630189f9892daf05
박민우 <alsd20177@gmail.com>
2023-12-28 17:00:12 +0900

    ✨ : #323 - middleware 설정 추가 => 시즌 아닐 때, 계획 수정 및 리마인드 수정 불가능 처리, 계획 상세 페이지 토큰 없이도 접근 가능 처리

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,43 +4,48 @@ import { checkIsSeason } from './utils/checkIsSeason';
 
 export function middleware(request: NextRequest) {
   const cookies = request.cookies;
-  const hasCookies = cookies.has('auth');
+  const hasAuthCookies = cookies.has('auth');
 
   if (request.nextUrl.pathname === '/') {
-    if (hasCookies) {
+    if (hasAuthCookies) {
       return NextResponse.redirect(new URL('/home', request.url));
     }
     return NextResponse.redirect(new URL('/login', request.url));
   } else if (request.nextUrl.pathname === '/home') {
-    if (!hasCookies) {
+    if (!hasAuthCookies) {
       return NextResponse.redirect(new URL('/login', request.url));
     }
-  } else if (
-    request.nextUrl.pathname === '/create' ||
-    request.nextUrl.pathname.startsWith('/edit')
-  ) {
-    if (!hasCookies) {
+  } else if (request.nextUrl.pathname === '/login') {
+    if (hasAuthCookies) {
+      return NextResponse.redirect(new URL('/home', request.url));
+    }
+  } else if (request.nextUrl.pathname === '/create') {
+    if (!hasAuthCookies) {
       return NextResponse.redirect(new URL('/login', request.url));
     }
-
     if (!checkIsSeason()) {
       return NextResponse.redirect(new URL('/home', request.url));
     }
   } else if (request.nextUrl.pathname === '/my') {
-    if (!hasCookies) {
+    if (!hasAuthCookies) {
       return NextResponse.redirect(new URL('/login', request.url));
-    }
-  } else if (request.nextUrl.pathname === '/login') {
-    if (hasCookies) {
-      return NextResponse.redirect(new URL('/home', request.url));
     }
   } else if (request.nextUrl.pathname.startsWith('/plans')) {
-    if (!hasCookies) {
-      return NextResponse.redirect(new URL('/login', request.url));
+    if (
+      request.nextUrl.pathname.startsWith('/plans/edit') &&
+      (!hasAuthCookies || !checkIsSeason())
+    ) {
+      return NextResponse.redirect(new URL('/home', request.url));
     }
   } else if (request.nextUrl.pathname.startsWith('/reminds')) {
-    if (!hasCookies) {
+    if (!hasAuthCookies) {
       return NextResponse.redirect(new URL('/login', request.url));
+    }
+    if (
+      request.nextUrl.pathname.startsWith('/reminds/edit') &&
+      !checkIsSeason()
+    ) {
+      return NextResponse.redirect(new URL('/home', request.url));
     }
   }
 
