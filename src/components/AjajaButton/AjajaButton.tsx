7e2ee7e2e965cc6c429b3fccc378f9875049b4ae
Ahn Hyesu <linim@naver.com>
2023-12-29 21:41:05 +0900

    ✨ : #347 - 로그인 하지 않았을 때 아좌좌 버튼 클릭하면 로그인 유도 모달 추가

--- src/components/AjajaButton/AjajaButton.tsx
+++ src/components/AjajaButton/AjajaButton.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { Icon } from '@/components';
+import { Icon, Modal, ModalBasic } from '@/components';
 import { QUERY_KEY } from '@/constants/queryKey';
 import { usePostAjajaMutation } from '@/hooks/apis/usePostAjajaMutation';
 import { useDebounce } from '@/hooks/useDebounce';
+import { useIsLogIn } from '@/hooks/useIsLogIn';
 import { useQueryClient } from '@tanstack/react-query';
 import classNames from 'classnames';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import type { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import './index.scss';
@@ -26,14 +28,30 @@ export default function AjajaButton({
   ...props
 }: AjajaButtonProps) {
   const queryClient = useQueryClient();
+  const router = useRouter();
   const [count, setCount] = useState(ajajaCount);
   const [fill, setFill] = useState(isFilled);
   const [originalCopy, setOriginalCopy] = useState(isFilled);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const { isLogin } = useIsLogIn();
   const { mutate: postAjaja } = usePostAjajaMutation();
+
+  const handleModalClickYes = () => {
+    router.push('/login');
+  };
+
+  const handleModalClickNo = () => {
+    setIsOpenModal(false);
+  };
+
   const handleAjaja = () => {
-    if (fill) setCount(count - 1);
-    else setCount(count + 1);
-    setFill(!fill);
+    if (isLogin) {
+      if (fill) setCount(count - 1);
+      else setCount(count + 1);
+      setFill(!fill);
+    } else {
+      setIsOpenModal(true);
+    }
   };
 
   const compare = () => {
@@ -57,22 +75,34 @@ export default function AjajaButton({
   }, [ajajaCount]);
 
   return (
-    <button
-      className={classNames(
-        `ajaja-button`,
-        `color-origin-text-100`,
-        `font-size-sm`,
-        classNameList,
+    <>
+      <button
+        className={classNames(
+          `ajaja-button`,
+          `color-origin-text-100`,
+          `font-size-sm`,
+          classNameList,
+        )}
+        onClick={handleAjaja}
+        {...props}>
+        <Icon
+          name="AJAJA"
+          size="lg"
+          color={fill ? 'primary' : 'secondary'}
+          isFilled={fill}
+        />
+        <p className={classNames('ajaja-p')}>아좌좌 {count}개</p>
+      </button>
+      {isOpenModal && (
+        <Modal>
+          <ModalBasic
+            onClickYes={handleModalClickYes}
+            onClickNo={handleModalClickNo}
+            confirmSentense="로그인 하기">
+            로그인이 필요한 기능입니다. 로그인 하시겠습니까?
+          </ModalBasic>
+        </Modal>
       )}
-      onClick={handleAjaja}
-      {...props}>
-      <Icon
-        name="AJAJA"
-        size="lg"
-        color={fill ? 'primary' : 'secondary'}
-        isFilled={fill}
-      />
-      <p className={classNames('ajaja-p')}>아좌좌 {count}개</p>
-    </button>
+    </>
   );
 }
