a0c6bca63d7fe0b1a3da64404ca6fe253c9726c1
nocheol <qkdl60@gmail.com>
2024-01-08 00:16:41 +0900

    ✨ : #404 - 이전 페이지 session 저장

--- src/components/RouteChangeObserver/RouteChangeObserver.tsx
+++ src/components/RouteChangeObserver/RouteChangeObserver.tsx
@@ -1,13 +1,19 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function RouteChangeObserver() {
   const pathName = usePathname();
-
+  const previousPage = useRef<string>('');
   useEffect(() => {
     console.log(pathName);
+    console.log('pre:' + previousPage.current);
+    if (pathName === '/login') {
+      window.sessionStorage.setItem('pre', previousPage.current);
+    } else {
+      previousPage.current = window.location.href;
+    }
   }, [pathName]);
   return <></>;
 }
