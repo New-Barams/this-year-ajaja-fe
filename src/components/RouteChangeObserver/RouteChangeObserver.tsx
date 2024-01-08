df55c28b7e7639c9f4db1927670d4aee5ee048a1
nocheol <qkdl60@gmail.com>
2024-01-09 00:40:30 +0900

    ♻️ : #404 - 로그인시 페이지 상관없이 이동

--- src/components/RouteChangeObserver/RouteChangeObserver.tsx
+++ src/components/RouteChangeObserver/RouteChangeObserver.tsx
@@ -6,19 +6,13 @@ import { useEffect, useRef } from 'react';
 export default function RouteChangeObserver() {
   const pathName = usePathname();
   const previousPage = useRef<string | null>(null);
-  //TODO: 세션 유틸로 변경, 로그인 후에도 작동되는데 괜찮은가?
-  //로그인 안 한 경우 접근 경로가 계획에서 오거나, 둘러보기에서 오거나, 계획갔다 둘러보기 가면 저장된다. 계획 둘러보기 로그인이어도 계획으로 간다 .
   useEffect(() => {
     if (pathName === '/login') {
       if (previousPage.current) {
         window.sessionStorage.setItem('prev', previousPage.current);
-      } else {
-        window.sessionStorage.removeItem('prev');
       }
-    } else if (pathName.startsWith('/plans')) {
-      previousPage.current = pathName;
     } else {
-      previousPage.current = null;
+      previousPage.current = pathName;
     }
   }, [pathName]);
   return <></>;
