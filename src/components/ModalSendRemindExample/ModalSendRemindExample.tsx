dcc49d283924b6dd8092a2088c18274a9a23d9b2
박민우 <alsd20177@gmail.com>
2024-01-10 14:54:51 +0900

    💄 : #397 - ModalSendRemindExample 컴포넌트 기본 디자인 구현

--- src/components/ModalSendRemindExample/ModalSendRemindExample.tsx
+++ src/components/ModalSendRemindExample/ModalSendRemindExample.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import classNames from 'classnames';
+import Image from 'next/image';
 import React from 'react';
 import { Modal, ModalBasic } from '..';
 import './index.scss';
+import remind_example_email_temp from '/public/remind_example_email_temp.png';
+import remind_example_kakao_temp from '/public/remind_example_kakao_temp.png';
 
 interface ModalSendRemindExampleProps {
   closeModal: () => void;
@@ -28,23 +32,63 @@ export default function ModalSendRemindExample({
           closeModal();
         }}
         confirmSentense="체험하기">
-        <p>리마인드 메세지란?</p>
+        <p className={classNames(['remind-example__title', 'font-size-lg'])}>
+          리마인드 메세지란?
+        </p>
 
-        <p>미래의 나에게 보내는 메세지에요.</p>
+        <p className="remind-example__description">
+          미래의 나에게 보내는 메세지에요.
+        </p>
 
-        <div>
-          <div>
-            <p>카카오톡</p>
-            <img />
+        <div className="remind-example__content">
+          <div className="remind-example__kakao">
+            <p
+              className={classNames([
+                'remind-example__kakao__title',
+                'font-size-xs',
+              ])}>
+              카카오톡
+            </p>
+            <Image
+              className="remind-example__kakao__image"
+              src={remind_example_kakao_temp}
+              alt="리마인드 메세지 예시 - 카카오톡"
+            />
           </div>
-          <div>
-            <p>이메일</p>
-            <img />
+
+          <div className="remind-example__email">
+            <p
+              className={classNames([
+                'remind-example__email__title',
+                'font-size-xs',
+              ])}>
+              이메일
+            </p>
+            <Image
+              className="remind-example__email__image"
+              src={remind_example_email_temp}
+              alt="리마인드 메세지 예시 - 이메일"
+            />
           </div>
         </div>
 
-        <p>설정된 알림 방식으로 체험해볼 수 있어요!</p>
-        <p>{'(1일 3번 제한)'}</p>
+        <div className="remind-example__try">
+          <p
+            className={classNames([
+              'remind-example__try__title',
+              'font-size-sm',
+            ])}>
+            설정된 알림 방식으로 체험해볼 수 있어요!
+          </p>
+          <p
+            className={classNames([
+              'remind-example__try__warning-text',
+              'font-size-sm',
+              'color-origin-primary',
+            ])}>
+            {'(1일 3번 제한)'}
+          </p>
+        </div>
       </ModalBasic>
     </Modal>
   );
