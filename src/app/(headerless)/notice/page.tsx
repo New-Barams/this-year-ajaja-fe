a8d662ec965d954610a8e16ebb6d6436e927b84f
박민우 <alsd20177@gmail.com>
2024-01-22 15:58:21 +0900

    ✨ : #416 - 공지사항 페이지 기본 UI 구현

---
+++ src/app/(headerless)/notice/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.scss';
+
+export default function NoticePage() {
+  return (
+    <div>
+      <h1 className="notice-page font-size-xl">공지사항</h1>
+    </div>
+  );
+}
