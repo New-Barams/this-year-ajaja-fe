2993e3991ce2a7a8c08a071eb0e756f2f1d20e38
박민우 <alsd20177@gmail.com>
2024-01-30 17:01:34 +0900

    ♻️ : #449 - constants index 파일 export 정리

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,17 +1,23 @@
-export { NETWORK } from './api';
-export { DOMAIN } from './api';
-export { COLOR } from './color';
-export { SESSION_STORAGE_KEY } from './createPlanSessionKey';
-export { STEP_NAME } from './createPlanStepTitle';
-export { EDIT_REMIND_STEP_TITLE } from './editRemindStepTitle';
-export { KAKAO_LOGIN_URL, UN_AUTH_URL } from '@/constants/login';
-export { planIcons } from './planIcons';
-export { QUERY_KEY } from './queryKey';
+export { NETWORK } from '@/constants/api';
+export { DOMAIN } from '@/constants/api';
+export { COLOR } from '@/constants/color';
+export { COOKIE_MAX_AGE } from '@/constants/cookie';
+export { SESSION_STORAGE_KEY } from '@/constants/createPlanSessionKey';
+export { STEP_NAME } from '@/constants/createPlanStepTitle';
+export { EDIT_REMIND_STEP_TITLE } from '@/constants/editRemindStepTitle';
+export {
+  KAKAO_LOGIN_URL,
+  UN_AUTH_URL,
+  KAKAO_LOGOUT_URL,
+} from '@/constants/login';
+export { maxPlan } from '@/constants/plan';
+export { planIcons } from '@/constants/planIcons';
+export { QUERY_KEY } from '@/constants/queryKey';
 export {
   TOTAL_PERIOD_OPTIONS,
   TERM_OPTIONS,
   DATE_OPTIONS,
   TIME_OPTIONS,
-} from './remindOptions';
-export { REMIND_TIME_TEXT } from './remindTimeText';
-export { INPUT_MAX_LENGTH } from './userInputMaxLength';
+} from '@/constants/remindOptions';
+export { REMIND_TIME_TEXT } from '@/constants/remindTimeText';
+export { INPUT_MAX_LENGTH } from '@/constants/userInputMaxLength';
