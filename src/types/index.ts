1d5fd2bf5a40fc60424f346d609c901aea2a5e80
Ahn Hyesu <linim@naver.com>
2024-01-31 18:46:57 +0900

    ♻️ : #452 - types 정리

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,14 @@
-export type { FontSize } from './FontSize';
-export type { Color } from './Color';
-export type { IconName } from './IconName';
-export type { PlanIconName } from './PlanIconName';
+export type { FontSize } from '@/types/FontSize';
+export type { Color } from '@/types/Color';
+export type { IconName } from '@/types/IconName';
+export type { PlanIconName } from '@/types/PlanIconName';
+export type { FeedbackItemData } from '@/types/Feedbacks';
+export type { PlanContentType } from '@/types/Plan';
+export type {
+  RemindOptionObjectType,
+  RemindOptionType,
+  RemindItemType,
+  ReadOnlyRemindItemData,
+  RemindData,
+} from '@/types/Remind';
+export type { SvgProps } from '@/types/Svg';
