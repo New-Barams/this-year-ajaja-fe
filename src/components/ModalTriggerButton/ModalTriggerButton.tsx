98b1d42c647aa6145df054223fffc1a9d10b9531
nocheol <qkdl60@gmail.com>
2024-02-28 23:47:00 +0900

    ✨ : #485 - ModalTriggerButton 추가

---
+++ src/components/ModalTriggerButton/ModalTriggerButton.tsx
@@ -0,0 +1,33 @@
+import React, { ReactElement, ReactNode, useState } from 'react';
+import { Modal } from '..';
+
+interface ModalTriggerButton {
+  className?: string; //외부에서 스타일적용을 위해서 className을 받아준다.
+  children: ReactNode;
+  renderModalContent: (onClickNo: () => void) => ReactElement; //렌더링해야하는 컴포넌트를 반환하는 함수
+}
+
+export default function ModalTriggerButton({
+  className,
+  renderModalContent,
+  children,
+}: ModalTriggerButton) {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false); //모달 상태
+  // 모달 여는 함수
+  const handleSetIsOpen = () => {
+    setIsModalOpen(true);
+  };
+  // 모달 닫는 함수
+  const handleSetIsClose = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <div className={className} onClick={handleSetIsOpen}>
+        {children}
+      </div>
+      {isModalOpen && <Modal>{renderModalContent(handleSetIsClose)}</Modal>}
+    </>
+  );
+}
