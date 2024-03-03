26bd9d6274fa1d7a4a7cd1f451b67675e0871935
nocheol <qkdl60@gmail.com>
2024-03-03 09:53:00 +0900

    ✨ : #485 - modal portal 추가

--- src/components/ModalTriggerButton/ModalTriggerButton.tsx
+++ src/components/ModalTriggerButton/ModalTriggerButton.tsx
@@ -1,15 +1,20 @@
+'use client';
+
 import React, { ReactElement, ReactNode, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { Modal } from '..';
 
 interface ModalTriggerButton {
   className?: string; //외부에서 스타일적용을 위해서 className을 받아준다.
   children: ReactNode;
+  container: HTMLDivElement | null;
   renderModalContent: (onClickNo: () => void) => ReactElement; //렌더링해야하는 컴포넌트를 반환하는 함수
 }
 
 export default function ModalTriggerButton({
   className,
   renderModalContent,
+  container,
   children,
 }: ModalTriggerButton) {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false); //모달 상태
@@ -27,7 +32,11 @@ export default function ModalTriggerButton({
       <div className={className} onClick={handleSetIsOpen}>
         {children}
       </div>
-      {isModalOpen && <Modal>{renderModalContent(handleSetIsClose)}</Modal>}
+      {isModalOpen &&
+        createPortal(
+          <Modal>{renderModalContent(handleSetIsClose)}</Modal>,
+          container!,
+        )}
     </>
   );
 }
