82222ea40e9e5def1b88657652376c48a8356d2f
nocheol <qkdl60@gmail.com>
2024-03-19 23:58:34 +0900

    ♻️ : #490 - 발자취 링크적용

--- src/app/main/@footprint/page.tsx
+++ src/app/main/@footprint/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import './index.scss';
 
 const footprints = [
@@ -13,16 +14,16 @@ const footprints = [
   { title: '나는 왜이렇게 살았는가!@', date: '2020.02.20' },
 ];
 // TODO d아이템 클릭시 해당 발자취로 이동
-export default function page() {
+export default function Page() {
   return (
     <div className="footprint__container">
       {footprints.map((footprint) => (
-        <div key={footprint.title} className="footprint__item">
+        <Link href={''} key={footprint.title} className="footprint__item">
           <span className="footprint__item--title">{footprint.title}</span>
           <span className="footprint__item--date color-origin-text-300 font-size-sm">
             {footprint.date}
           </span>
-        </div>
+        </Link>
       ))}
     </div>
   );
