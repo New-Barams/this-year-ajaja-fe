2ec75bd568efdd11ce553793d974b0da3196111b
nocheol <qkdl60@gmail.com>
2024-03-20 00:04:05 +0900

    ♻️ : #490 - 헤더 여백 정리

--- src/app/main/@createButtons/page.tsx
+++ src/app/main/@createButtons/page.tsx
@@ -2,9 +2,9 @@ import { Icon } from '@/components';
 import Link from 'next/link';
 import './index.scss';
 
-export default function page() {
+export default function Page() {
   return (
-    //TODO 버튼 활성화 여부 적용해주고 안되면 toast처리
+    //TODO 작성 가능 여부 확인 후 rout 처리 안되면 toast처리
     <div className="create-buttons__container">
       <Link href={'/'} className="outside-box create-buttons__button">
         <h1 className="create-buttons__button--name">계획 작성{' >'}</h1>
